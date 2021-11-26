ef7e388bf48f869d29c49c8392795ae9bb6df300
Eva Chu <echu58@my.bcit.ca>
2021-11-25 23:11:15 -0800

    created functions cleanUpView and renderView for View Page

--- script.js
+++ script.js
@@ -17,22 +17,24 @@ function createSingleIndex(contact) {
     // this function will take in a single object,
     // where that object represents a single contact (see examples below)
     // It should output a DOM node, does not need to attach the DOM node to the rest of the DOM
-    
-    let elementA = document.createElement("a")
-    elementA.setAttribute("href", "page3.html")
+    if (document.title == "Contacts") {
 
-    let elementDiv = document.createElement("div")
-    elementDiv.className = "contact"
+        let elementA = document.createElement("a")
+        elementA.setAttribute("href", "page3.html")
 
-    let elementP = document.createElement("p")
+        let elementDiv = document.createElement("div")
+        elementDiv.className = "contact"
 
-    let newContent = document.createTextNode(contact["name"])
-    
-    elementP.appendChild(newContent)
-    elementDiv.appendChild(elementP)
-    elementA.appendChild(elementDiv)
-    
-    return elementA
+        let elementP = document.createElement("p")
+
+        let newContent = document.createTextNode(contact["name"])
+        
+        elementP.appendChild(newContent)
+        elementDiv.appendChild(elementP)
+        elementA.appendChild(elementDiv)
+        
+        return elementA
+    }
 }
 
 
@@ -49,19 +51,20 @@ function renderIndex(contacts) {
     // which is an array containing many contacts,
     // each of which represents a single contact.
     // this function must actually put DOM nodes onto the web page
-    
-    let docParent = document.querySelector(".main")
-    
-    for (i = 0; i < contacts.length; i++) {
+    if (document.title == "Contacts") {
+        let docParent = document.querySelector(".main")
         
-        let contact = contacts[i]
+        for (i = 0; i < contacts.length; i++) {
+            
+            let contact = contacts[i]
 
-        docParent.appendChild(createSingleIndex(contact))
+            docParent.appendChild(createSingleIndex(contact))
 
-        // for my own clarity when testing:
-        // console.log(`added ${contact.name}`)
-    }   
-    // console.log(`added ${contacts.length} contacts`)
+            // for my own clarity when testing:
+            // console.log(`added ${contact.name}`)
+        }   
+        // console.log(`added ${contacts.length} contacts`)
+    }
 }
 
 
@@ -80,3 +83,74 @@ let contactList = [
       email: "whatsup@doc.example.com",
     }
   ]
+
+
+
+// View Page
+
+
+  function cleanUpView() {
+    // removes all of the DOM nodes that are unique to the View page
+    if (document.title == "Contact Name") {
+        parent = document.querySelector(".main")
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild)
+        }
+    }
+}
+
+
+function renderView(contact) {
+    // creates all of the DOM nodes that are unique to the View page
+    // function will take in a single parameter,
+    // which is an object that represents a single contact
+    // this function must actually modify web page
+    if (document.title == "Contact Name") {
+        let docParent = document.querySelector(".main")
+        
+        let elementDivContactInfo = document.createElement("div")
+        elementDivContactInfo.className = "contactinfo"
+
+        let elementDivContactName = document.createElement("div")
+        elementDivContactName.className = "contactname"
+        elementDivContactName.textContent = ` ${contact.name} `
+
+        let elementImg = document.createElement("img")
+        elementImg.setAttribute("src", "./img/profile.jpg")
+        elementImg.className = "profilepic"
+        elementImg.setAttribute("alt", "Profile picture")
+
+        elementDivContactName.appendChild(elementImg)
+
+        let elementDivContactEmail = document.createElement("div")
+        elementDivContactEmail.className = "contactemail"
+        elementDivContactEmail.textContent = `email: ${contact.email}`
+
+        let elementDivContactPhone = document.createElement("div")
+        elementDivContactPhone.className = "contactphone"
+        elementDivContactPhone.textContent = `cell: ${contact.phone}`
+
+        let elementDivContactAddress = document.createElement("div")
+        elementDivContactAddress.className = "contactaddress"
+        elementDivContactAddress.textContent = `address: ${contact.address}`
+
+        let elementDivButtons = document.createElement("div")
+        elementDivButtons.className = "buttons"
+
+        let elementButtonEdit = document.createElement("button")
+        elementButtonEdit.className = "button edit"
+        elementButtonEdit.value = "Edit"
+        elementButtonEdit.textContent = "Edit"
+
+        let elementButtonClose = document.createElement("button")
+        elementButtonClose.className = "button close"
+        elementButtonClose.value = "Close"
+        elementButtonClose.textContent = "Close"
+
+        elementDivButtons.append(elementButtonEdit, elementButtonClose)
+
+        elementDivContactInfo.append(elementDivContactName, elementDivContactEmail, elementDivContactPhone, elementDivContactAddress, elementDivButtons)
+        
+        docParent.appendChild(elementDivContactInfo)
+    }
+}
